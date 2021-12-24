62e451c13353128be5666637208426b6c492dd6d
lunz0223 <lunz@uchicago.edu>
2021-12-24 11:14:28 -0600

    Mongodb dependency and config

--- tiktok-dev/tiktok-api/src/main/java/com/lunz/Application.java
+++ tiktok-dev/tiktok-api/src/main/java/com/lunz/Application.java
@@ -3,12 +3,14 @@ package com.lunz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.stereotype.Component;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.lunz.mapper")
 @ComponentScan(basePackages = {"com.lunz","org.n3r.idworker"})
+@EnableMongoRepositories
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
